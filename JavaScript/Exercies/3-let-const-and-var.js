e5263061a74d574dc848df0edafc1cd8ad488c0b
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-08-07 11:33:02 +0300

    uploading JS exercise let, const and var

---
+++ JavaScript/Exercies/3-let-const-and-var.js
@@ -0,0 +1,15 @@
+"use strict";
+/*
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
+*/
+const isIsland = false;
+const language = "bulgarian";
+const country = "Bulgaria";
+const continent = "Europe";
+let population = 5000000;
+
+console.log(language);

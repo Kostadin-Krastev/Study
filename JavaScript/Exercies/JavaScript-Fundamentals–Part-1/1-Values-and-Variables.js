42c138b7ffabdda2f2d28f960334af21b2c43341
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-08-07 11:10:44 +0300

    uploading javascript exercise Values and Variables

---
+++ JavaScript/Exercies/JavaScript-Fundamentals–Part-1/1-Values-and-Variables.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+const country = "Bulgaria";
+const continent = "Europe";
+let population = 5000000;
+
+console.log(country, continent, population);

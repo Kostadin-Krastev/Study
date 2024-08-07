566b0cc27ad4cc8de16c5c27287d6ceec57e2480
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-08-07 14:18:11 +0300

    uploading JS exercise Logical operators

--- JavaScript/Exercies/JavaScript-Fundamentals–Part-1/8-Logical-Operators.js
+++ JavaScript/Exercies/JavaScript-Fundamentals–Part-1/8-Logical-Operators.js
@@ -25,7 +25,7 @@ island.
 'Portugal does not meet your criteria :('
 */
 
-if (language === "english" && population < 50000000 && numNeighbours > 0) {
+if (language === "english" && population < 50000000 !== isIsland) {
   console.log(`You should live in ${country}`);
 } else {
   console.log(`${country} does not meet your criteria`);

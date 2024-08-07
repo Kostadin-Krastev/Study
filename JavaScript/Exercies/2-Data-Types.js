c4f4f090bd1b493162a848ffb43bde1fc7fad3d7
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-08-07 11:20:54 +0300

    uploading JS exercise Data types

---
+++ JavaScript/Exercies/2-Data-Types.js
@@ -0,0 +1,14 @@
+"use strict";
+/*
+1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console
+*/
+// Task 1:
+const isIsland = false;
+let language;
+
+// Task 2:
+const country = "Bulgaria";
+let population = 5000000;
+
+console.log(isIsland, population, country, language);

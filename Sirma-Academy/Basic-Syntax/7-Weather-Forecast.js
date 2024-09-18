bd36712b4fc8a51e5d6d9129bcaee38baa834c1d
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-09-19 00:04:22 +0300

    JS exercise Weather forecast

---
+++ Sirma-Academy/Basic-Syntax/7-Weather-Forecast.js
@@ -0,0 +1,9 @@
+/*
+Write a function that takes as parameters city (text) and degrees (integer) and displays the following message on the console: "Today in {city} it is {degrees} degrees.
+ */
+
+function weathercast(city, numDegrees) {
+  console.log("Today in " + city + " it is " + numDegrees + " degrees.");
+}
+
+weathercast("Sofia", 29);

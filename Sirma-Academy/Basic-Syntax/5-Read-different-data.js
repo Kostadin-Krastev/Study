0a2653287579990fce5922f9155af9172ec187ae
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-09-18 23:58:15 +0300

    JS exercise Different data

---
+++ Sirma-Academy/Basic-Syntax/5-Read-different-data.js
@@ -0,0 +1,12 @@
+/*
+Write a program that receives 3 parameters:
+- Text 
+- Integer 
+- Real number 
+ */
+
+function differentData(Text, Integer, realNumber) {
+  console.log(Text, Integer, realNumber);
+}
+
+differentData("Ivan", 100, 3.14);

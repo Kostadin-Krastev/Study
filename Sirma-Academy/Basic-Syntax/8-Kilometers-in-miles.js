196c1e29d55ac8bbb5dc68a2ddd63506107b5331
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-09-19 00:11:19 +0300

    JS exercise converting kilometers into miles

---
+++ Sirma-Academy/Basic-Syntax/8-Kilometers-in-miles.js
@@ -0,0 +1,11 @@
+/*
+Write a function that takes a parameter kilometer (real number) and converts them to miles. Print the result on the console. 
+Note 1 kilometer = 0.621371 miles. 
+ */
+
+function kmIntoMils(kilometers) {
+  const miles = kilometers * 0.621371;
+  console.log(miles);
+}
+
+kmIntoMils(10.5);

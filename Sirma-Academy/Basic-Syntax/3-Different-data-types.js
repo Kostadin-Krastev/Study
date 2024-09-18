d29b1ec9254f18a737b4f0e946677997c822fd82
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-09-18 23:48:04 +0300

    JS exercise different data types

---
+++ Sirma-Academy/Basic-Syntax/3-Different-data-types.js
@@ -0,0 +1,12 @@
+/*
+Create a variable to which you assign a value of 100. Print the value of the variable.
+Create a variable to which you assign a value of 3.15. Print the value of the variable.
+ */
+
+function printNumbers() {
+  const num = 100;
+  const num2 = 3.15;
+  console.log(num, num2);
+}
+
+printNumbers();

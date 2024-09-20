66dd691da1d2784f4bd1e3d69c31de28686242d7
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-09-20 09:26:36 +0300

    JS exercise Square face

---
+++ Sirma-Academy/Basic-Syntax/10-Square-face.js
@@ -0,0 +1,12 @@
+/*
+Write a function that takes integer 'a’ and calculates the square area with side 'a'. Guidelines:
+Initialize an area variable in which to record the value for the rectangle face obtained
+by the side*side formula. Print the result.
+ */
+
+function calcSquareArea(numA) {
+  const area = numA * numA;
+  console.log(area);
+}
+
+calcSquareArea(10);

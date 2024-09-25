f50e71b1ddee31be266991e2112f1ad0f4152a8f
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-09-25 10:19:08 +0300

    JS exercise Numbers from 0 to 100

---
+++ Sirma-Academy/3-Loops/1-Numbers-from-0-to-100.js
@@ -0,0 +1,12 @@
+"use strict";
+/*
+Write a function that prints the numbers from 1 to 100, each on a new line.
+ */
+
+function printingNumbers(numbers) {
+  for (numbers = 1; numbers <= 100; numbers++) {
+    console.log(numbers);
+  }
+}
+
+printingNumbers(1);

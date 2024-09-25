4ca7b29d835d757e5a55a714a9881ae6cb6f7e2a
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-09-25 19:33:21 +0300

    JS exercise introducing to objects

---
+++ JavaScript/Exercies/JavaScript-Fundamentals–Part-2/7-Introduction-to-Objects.js
@@ -0,0 +1,14 @@
+"use strict";
+/*
+Create an object called 'myCountry' for a country of your choice, containing
+properties 'country', 'capital', 'language', 'population' and
+'neighbours' (an array like we used in previous assignments)
+ */
+
+const myCountry = {
+  country: "Bulgaria",
+  capital: "Sofia",
+  language: "bulgarian",
+  population: 5000000,
+  neighbours: ["Romania", "Serbia", "Macedonia", "Greece", "Turkey"],
+};

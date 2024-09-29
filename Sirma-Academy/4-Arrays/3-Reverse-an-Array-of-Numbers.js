ad763092746ef2d974066f439e6cff1d06f93191
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-09-29 22:52:05 +0300

    Js exercise "Reverse an array of numbers

---
+++ Sirma-Academy/4-Arrays/3-Reverse-an-Array-of-Numbers.js
@@ -0,0 +1,26 @@
+/*
+Receive a number n and an array of elements, create a new array with n numbers,
+reverse it and print its elements on a single line, space separated.
+ */
+
+let numberInput = 3;
+let arrOfNumbers = [10, 20, 30, 40, 50];
+
+// Test data 2
+numberInput = 4;
+arrOfNumbers = [-1, 20, 99, 5];
+
+// Test data 3
+numberInput = 2;
+arrOfNumbers = [66, 43, 75, 89, 47];
+
+function reverseArray(numberInput, arrOfNumbers) {
+  let newArray = [];
+
+  for (i = numberInput - 1; i >= 0; i--) {
+    newArray.push(arrOfNumbers[i]);
+  }
+  console.log(newArray.join(" "));
+}
+
+reverseArray(numberInput, arrOfNumbers);

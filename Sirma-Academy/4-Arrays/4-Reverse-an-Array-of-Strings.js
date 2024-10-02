3557e8ca6aabf245051f8e9c6f0360141c8b36e4
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-10-02 21:19:38 +0300

    Js exercise "Revers an array of strings"

--- Sirma-Academy/4-Arrays/4-Reverse-an-Array-of-Strings.js
+++ Sirma-Academy/4-Arrays/4-Reverse-an-Array-of-Strings.js
@@ -1,23 +1,33 @@
-"use strict";
+'use strict';
 /*
 Receive an array of strings (space separated values), reverse it, and print its elements.
 Swap elements. For example, the first element should be last, and the last element
 should be first etc.
  */
 
-let arrStrings = ["a", "b", "c", "d", "e"];
-// Test data 2
-arrStrings = ["abc", "def", "hig", "klm", "nop"];
-// Test data 3
-arrStrings = ["33", "123", "0", "dd"];
+let arrStrings = ['a', 'b', 'c', 'd', 'e'];
+// // Test data 2
+arrStrings = ['abc', 'def', 'hig', 'klm', 'nop'];
+// // Test data 3
+arrStrings = ['33', '123', '0', 'dd'];
 
 function reversArray(arrStrings) {
-  let reverseStringArray = [];
+  for (let i = 0; i < arrStrings.length; i++) {
+    // Stopping the loop from overscrolling
+    if (i >= arrStrings.length) {
+      break;
+    }
 
-  for (let i = arrStrings.length; i >= 0; i--) {
-    reverseStringArray.push(arrStrings[i]);
+    // Saving the first element
+    let firstElement = arrStrings[i];
+
+    // Swapping the first element
+    arrStrings[i] = arrStrings[arrStrings.length - 1 - i];
+    // Swapping the last element
+    arrStrings[arrStrings.length - 1 - i] = firstElement;
   }
-  console.log(reverseStringArray.join(" "));
+
+  console.log(arrStrings.join(' '));
 }
 
 reversArray(arrStrings);

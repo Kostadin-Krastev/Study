47d4f8186f6dbc0684316d04bee5b98f565a811d
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-12-13 23:38:11 +0200

    js file from DOM exercise Task 1 that contain teacher solution

---
+++ Sirma-Academy/16-DOM-Introduction/16-Exercise-DOM-Introduction/1-Modify-Style-Teacher-solution/script.js
@@ -0,0 +1,9 @@
+function restyle() {
+  // Select element
+  const changeElementStyle = document.getElementById('text');
+
+  // Change style
+  changeElementStyle.style.fontFamily = 'Arial';
+  changeElementStyle.style.fontSize = '40px';
+  changeElementStyle.style.color = 'red';
+}

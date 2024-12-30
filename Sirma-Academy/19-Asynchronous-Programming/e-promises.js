8c37c7b5c312296db6ece264fec7a37985341ab6
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2024-12-30 23:22:11 +0200

    promises demo code from lecture Asynchronous programming

--- Sirma-Academy/19-Asynchronous-Programming/e-promises.js
+++ Sirma-Academy/19-Asynchronous-Programming/e-promises.js
@@ -13,3 +13,17 @@ weddingPromise
   .catch((reason) => console.log(`Wedding is of - ${reason}`)); // if the promise fail
 
 console.log('Promise is given');
+
+// Always resolve promise
+const resolvingPromise = Promise.resolve('Success');
+// resolvingPromise.then((res) => console.log(res));
+
+// All promises must resolve
+Promise.all([resolvingPromise, weddingPromise, 'Pesho'])
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => console.log(`All promises fails - ${err}`))
+  .finally(() => {
+    console.log('Finaly is always last');
+  });

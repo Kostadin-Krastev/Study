99db9712f4c19b068b9aa67edf2da15cc8b7e0f2
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-02-05 16:04:55 +0200

    exerciseing some javascript arrays and loops

---
+++ exarcise.js
@@ -0,0 +1,17 @@
+function sumArr(input) {
+  while (input.length > 1) {
+    let condenceArr = [];
+
+    for (let i = 0; i < input.length - 1; i++) {
+      let arrValue = input[i] + input[i + 1];
+
+      condenceArr.push(arrValue);
+
+      input = condenceArr;
+    }
+  }
+
+  console.log(input);
+}
+
+sumArr([2, 10, 3]);

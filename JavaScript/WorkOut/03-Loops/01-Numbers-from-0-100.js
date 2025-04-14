c1488c359ae8f1abcb5505d4ec07ea401ab8e6a4
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-04-14 16:22:54 +0300

    javascript exercise loops: printing numbers from 1 to 100

---
+++ JavaScript/WorkOut/03-Loops/01-Numbers-from-0-100.js
@@ -0,0 +1,11 @@
+/*
+Write a function that prints the numbers from 1 to 100, each on a new line.
+ */
+
+const printNumbers = function () {
+  for (let i = 1; i < 100 + 1; i++) {
+    console.log(i);
+  }
+};
+
+printNumbers();

48accf208b3dee2091a18411662de1787568c0c5
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-04-14 14:37:37 +0300

    javascript exercise some basic syntax

---
+++ JavaScript/WorkOut/01-Basic-syntax/05-Read-different-data.js
@@ -0,0 +1,12 @@
+/*
+Write a program that receives 3 parameters: 
+-Text 
+-Integer 
+-Real number
+ */
+
+const readDifferentDataFunction = function (text, integer, realNumber) {
+  console.log(text, integer, realNumber);
+};
+
+readDifferentDataFunction('Ivan', 100, 3.14);

673152a9c34a4ac2efecc1d90515303f6419ade7
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-04-30 14:17:25 +0300

    js exercise about conditional statements

---
+++ JavaScript/WorkOut/02-Conditional-Statements/05-Face-figures.js
@@ -0,0 +1,13 @@
+/*
+Write a function that takes the look and dimensions of a geometric figure and calculates
+its face.
+The figures are of four types: square, rectangle, circle, and triangle. The first
+parameter is the type of figure (square, rectangle, circle, or triangle). 
+- If the figure is a square, the next parameter is a number - the length of its side. 
+- If the figure is a rectangle, the next two parameters produce two numbers - the
+lengths of its sides. 
+- If the figure is a circle, the next parameter is a number - the radius of the circle. 
+- If the figure is a triangle, the next two parameters produce two numbers - the
+length of its side and the length of the height to it.
+Round the result to 2 decimal places.
+ */

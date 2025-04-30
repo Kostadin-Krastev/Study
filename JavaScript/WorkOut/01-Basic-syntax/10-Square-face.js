47ffd1295d4263fe6195f4ddd2d3fcaacece46b4
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-04-30 12:13:46 +0300

    js exercise about basic syntax

---
+++ JavaScript/WorkOut/01-Basic-syntax/10-Square-face.js
@@ -0,0 +1,11 @@
+/*
+Write a function that takes integer 'a’ and calculates the square area with side 'a'.
+ */
+
+const calculateSquare = function (a) {
+  const calculation = a * a;
+
+  console.log(calculation);
+};
+
+calculateSquare(10);

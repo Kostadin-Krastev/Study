8e04dde3c9c96161c12c217341b099c046a242a3
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-05-10 23:04:44 +0300

    js exercise about loops: print letters from text using for loop

---
+++ JavaScript/WorkOut/03-Loops/5-Letters-in-word.js
@@ -0,0 +1,11 @@
+/*
+Write a function that accepts text and prints each letter on a new line.
+ */
+
+const printLetters = function (inputText) {
+  for (let i = 0; i < inputText.length; i++) {
+    console.log(inputText[i]);
+  }
+};
+
+printLetters('Bulgaria');

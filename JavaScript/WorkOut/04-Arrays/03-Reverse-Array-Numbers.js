b9e0a205c450b8ae0b34bf003ff3230821326d05
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-05-25 12:32:56 +0300

    starting a js exercise for arrays

---
+++ JavaScript/WorkOut/04-Arrays/03-Reverse-Array-Numbers.js
@@ -0,0 +1,15 @@
+/*
+Receive a number n and an array of elements, create a new array with n numbers,
+reverse it and print its elements on a single line, space separated.
+ */
+
+function createNewArray(inputNumber, inputArray) {
+  let newArray;
+  for (let i = inputNumber; inputArray.length <= i; i++) {
+    newArray[i];
+  }
+
+  console.log(newArray);
+}
+
+createNewArray(3, [10, 20, 30, 40, 50]);

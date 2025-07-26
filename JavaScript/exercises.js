09b23b689d903f62ddaf118bb416c7bb8e799fcc
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-07-26 13:08:04 +0300

    js exercises

---
+++ JavaScript/exercises.js
@@ -0,0 +1,28 @@
+// Task 1
+let bulgariaPopulation = 6000000;
+const splitCountryPopulation = bulgariaPopulation / 2;
+
+console.log(splitCountryPopulation);
+
+// Task 2
+bulgariaPopulation++;
+
+console.log(bulgariaPopulation);
+
+// Task 3
+let finlandPopulation = 6000000;
+const compereCountreysPopulation = bulgariaPopulation > finlandPopulation;
+
+console.log(compereCountreysPopulation);
+
+// Task 4
+const averageCountryPopulation = 33000000;
+const compereAverageCountreysPopulation =
+  averageCountryPopulation < bulgariaPopulation;
+
+console.log(compereAverageCountreysPopulation);
+
+// Task 5
+const description = `Bulgaria is in Europe, and it's ${bulgariaPopulation} milion people speaks bulgarian`;
+
+console.log(description);

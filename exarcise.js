c6c8e3805e12327855d73be20669bc65523867b9
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-02-09 12:27:12 +0200

    finsh task

--- exarcise.js
+++ exarcise.js
@@ -5,3 +5,30 @@ hours later another car went to the same destination at a speed y km / h. How ma
 kilometers will be the distance between the two cars 3 hours after the departure of the
 second car?
  */
+
+/*
+destination Sofia - Berlin
+firstCar
+2 hours later
+secondCar
+
+how many kilometars will be the distance between the two cars after 3 hours
+*/
+let firstCarSpeed = 81;
+firstCarSpeed = 100;
+let secondCarSpeed = 120;
+secondCarSpeed = 90;
+
+const findeDistance = function (inputFirstCarSpeed, inputSecondCarSpeed) {
+  let timeHourse = 3;
+  // distance each car hase past
+  const firstCarDistance = inputFirstCarSpeed * (timeHourse + 2);
+  const secondCarDistance = inputSecondCarSpeed * timeHourse;
+
+  // distance between the two cars
+  const distance = `The distance between the two cars after ${timeHourse} hours will be ${firstCarDistance - secondCarDistance}`;
+
+  return distance;
+};
+
+console.log(findeDistance(firstCarSpeed, secondCarSpeed));

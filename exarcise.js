fafba753aad0dab57ad74a50422ee483e4c2d8a1
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-02-12 11:36:03 +0200

    task complited

--- exarcise.js
+++ exarcise.js
@@ -2,3 +2,10 @@
 /*
 Write a function that prints the numbers from 1 to 100, each on a new line.
 */
+const printNumbers = function () {
+  for (let numbers = 1; numbers <= 100; numbers++) {
+    console.log(numbers);
+  }
+};
+
+printNumbers();

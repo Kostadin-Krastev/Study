59b6cccc5df394e04230beaa43588b4be914db55
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-02-12 10:33:29 +0200

    first commit

--- exarcise.js
+++ exarcise.js
@@ -1,21 +1,4 @@
 'use strict';
 /*
-Write a program that receives speed (real number) and prints speed information.
+Write a function that prints the numbers from 1 to 100, each on a new line.
 */
-
-let speed = 10;
-speed = 59;
-speed = 120;
-speed = 121;
-
-if (speed <= 10) {
-  console.log('slow');
-} else if (speed > 10 && speed <= 60) {
-  console.log('average');
-} else if (speed > 60 && speed <= 120) {
-  console.log('fast');
-} else if (speed > 120 && speed <= 160) {
-  console.log('super-fast');
-} else {
-  console.log('turbo-fast');
-}

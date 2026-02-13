da97b78e67ce0073e46735695bc46e35a63e65f9
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-02-13 14:07:21 +0200

    task completed

--- exarcise.js
+++ exarcise.js
@@ -3,3 +3,13 @@
 Write a function that accepts a positive number n and prints the numbers from n to 1 in
 reverse order (from largest to smallest).
 */
+
+let number = 10;
+number = 5;
+
+const printNumbersRevesed = function (number) {
+  for (let i = number; i >= 1; i--) {
+    console.log(i);
+  }
+};
+printNumbersRevesed(number);

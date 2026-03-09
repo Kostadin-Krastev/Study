8c03aaa35377afaa3ed600a2f5884d89ffebb58f
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-03-09 21:40:29 +0200

    solve the task with if statement

--- exarcise.js
+++ exarcise.js
@@ -4,3 +4,15 @@
 Write a JS function which receive three integer numbers to print the smallest of the
 three integer numbers. Use appropriate name for the function.
  */
+
+const printSmallest = function (inputA, inputB, inputC) {
+  if (inputA < inputB && inputA < inputC) {
+    console.log(inputA);
+  } else if (inputB < inputA && inputB < inputC) {
+    console.log(inputB);
+  } else {
+    console.log(inputC);
+  }
+};
+
+printSmallest(600, 342, 123);

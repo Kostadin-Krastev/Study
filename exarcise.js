ea26b6b34828be24729a175117b09e18f13421f5
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-03-13 09:39:34 +0200

    first commit

--- exarcise.js
+++ exarcise.js
@@ -1,23 +1,6 @@
 'use strict';
 /*
-You will receive 3 integers. Write a JS function sum to get the sum of the first two
-integers and subtract function that subtracts the third integer from the result from the
-Sum function.
+Calculate the Body Mass Index (BMI) based on the given weight (in kilograms) and height (in
+meters).
+Use the formula BMI= Weight / Height2.
  */
-let firstInteger = 23;
-let secondInteger = 6;
-let thirdInteger = 10;
-
-const sum = function (inputA, inputB) {
-  const additionSum = inputA + inputB;
-
-  return additionSum;
-};
-
-const subtractSum = function (inputFunction, inputC) {
-  const subtractResult = inputFunction - inputC;
-
-  return subtractResult;
-};
-
-console.log(subtractSum(sum(firstInteger, secondInteger), thirdInteger));

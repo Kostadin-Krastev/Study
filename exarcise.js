6d44b718ff528a5d4ef12b5d337e3d364b6641f5
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-03-13 10:07:52 +0200

    task completed

--- exarcise.js
+++ exarcise.js
@@ -1,6 +1,26 @@
 'use strict';
 /*
-Calculate the Body Mass Index (BMI) based on the given weight (in kilograms) and height (in
-meters).
-Use the formula BMI= Weight / Height2.
+Calculate the daily water consumption per person in a household. Given the total water
+consumption in liters for a week and the number of people in the household, find out the daily
+consumption per person.
+
+Use the formula
+Daily consumption per person = Total weekly consumption / 7 * Number of people
  */
+
+// store water consumption and people in household
+let waterWeeklyConsumption = 2450;
+waterWeeklyConsumption = 3150;
+let numberPeople = 7;
+numberPeople = 10;
+
+const calcWaterConsumptionPerPerson = function (inputWater, inputPeople) {
+  // calculate the daily water consumption per person
+  const dailyWaterConsum = inputWater / (7 * inputPeople);
+
+  return dailyWaterConsum.toFixed(2);
+};
+
+console.log(
+  calcWaterConsumptionPerPerson(waterWeeklyConsumption, numberPeople),
+);

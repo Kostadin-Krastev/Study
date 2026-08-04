8254e6c55668ac45c1b7d33eb48316792337aed9
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-08-04 21:46:16 +0300

    new exarcise

--- exarcise.js
+++ exarcise.js
@@ -1,17 +1,9 @@
 'use strict';
 /*
-Write a function that prints the hours of the day from 0:0 to 23:59, each on a separate
-line. Hours must be written in the format '{hour}:{minutes}'.
+Calculate the average speed of a vehicle that has traveled a certain distance D in kilometers
+over a given time T in hours.
+Use the formula Average Speed = D / T.
+
+On the first line, you receive the distance D in kilometers.
+On the second line, you receive the time T in hours.
 */
-
-const clock = function () {
-  for (let hour = 0; hour <= 23; hour++) {
-    for (let minutes = 0; minutes < 60; minutes++) {
-      console.log(
-        `${String(hour).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`,
-      );
-    }
-  }
-};
-
-clock();

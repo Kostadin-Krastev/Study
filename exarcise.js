d7627efbaa29c378ab66e054ffb27acc56fc9804
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-08-04 21:52:13 +0300

    complete exarcise

--- exarcise.js
+++ exarcise.js
@@ -7,3 +7,18 @@ Use the formula Average Speed = D / T.
 On the first line, you receive the distance D in kilometers.
 On the second line, you receive the time T in hours.
 */
+
+let distanceKm = 200;
+distanceKm = 150;
+distanceKm = 300;
+let timeHours = 2;
+timeHours = 3;
+timeHours = 5;
+
+const calculateAverageSpeed = function (distanceKm, timeHours) {
+  const averageSpeed = distanceKm / timeHours;
+
+  return averageSpeed.toFixed(2);
+};
+
+console.log(calculateAverageSpeed(distanceKm, timeHours));

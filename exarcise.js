4167c5a858a8963daa579654c5be96b585d56c43
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-08-07 12:14:06 +0300

    complate js exarcise

--- exarcise.js
+++ exarcise.js
@@ -8,3 +8,15 @@ INPUT       OUTPUT
 7           43.96
 10          62.83
 */
+
+let radius = 5;
+radius = 7;
+radius = 10;
+
+const calculateCircumference = function (radius) {
+  const calculatingCircumferenceFormula = 2 * 3.14159 * radius;
+
+  return calculatingCircumferenceFormula.toFixed(2);
+};
+
+console.log(calculateCircumference(radius));
